b5ec791631684655028fc9dc9390bcdc9dbc1619
Jon Sharman <jon@freshworks.io>
2018-12-13 09:10:45 -0800

    Section3 Lecture 41 Complete.  Listening to onclick events and passing props to components

--- complete-react-guide/src/App.js
+++ complete-react-guide/src/App.js
@@ -2,11 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Max', age: 28},
+      {name: 'Manu', age: 29},
+      {name: 'Stephanie', age: 26}
+    ]
+  }
+
+  switchNameHandler = () =>{
+    console.log('was clicked');
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi I'm a react app</h1>
-        <Person />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> I code apps</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
     // return React.createElement('div',{className: 'App'}, React.createElement('h1', null,'I am a react app'));
